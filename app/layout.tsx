f3af66d285d3748997957a8ab065fe2a6ffdec15
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2024-06-05 21:29:21 +0100

    landing page inititl

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,21 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Libre_Franklin } from 'next/font/google'
+import { Chivo } from 'next/font/google'
+import { cn } from "@/lib/utils";
+
+const libre_franklin = Libre_Franklin({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-libre_franklin',
+});
+
+const chivo = Chivo({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-chivo',
+});
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(inter.className, libre_franklin.variable, chivo.variable)}>{children}</body>
     </html>
   );
 }
