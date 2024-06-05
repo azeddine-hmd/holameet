8fe3704f379640b91ff48abddd573c8546473a5a
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2024-06-05 19:35:14 +0100

    adding library

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import { Calendar } from "@/components/ui/calendar";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="bg-red-500">
-      </div>
+      <Calendar />
     </main>
   );
 }
