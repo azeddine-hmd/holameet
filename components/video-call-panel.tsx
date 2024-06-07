575991819807f9dd632f703d28df57b804413841
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2024-06-07 21:27:49 +0100

    video call control style complete

--- components/video-call-panel.tsx
+++ components/video-call-panel.tsx
@@ -14,11 +14,18 @@ export type VideoCallPanel = React.ComponentProps<"div">;
 
 export default function VideoCallPanel({ className, ...restProps }: VideoCallPanel) {
   const { ref, width } = useResizeObserver<HTMLDivElement>();
-  const [ isMicMute, setMicMute ] = useState(false);
-  const [ isHeadsetMute, setHeadsetMute ] = useState(false);
+  const [isMicMute, setMicMute] = useState(false);
+  const [isHeadsetMute, setHeadsetMute] = useState(false);
+  const [hideCallControl, setHideCallControl] = useState(false);
 
   return (
-    <div ref={ref} className={cn("w-full h-full relative", className)} {...restProps}>
+    <div
+      ref={ref}
+      className={cn("w-full h-full relative", className)}
+      onMouseEnter={() => setHideCallControl(false)}
+      onMouseLeave={() => setHideCallControl(true)}
+      {...restProps}
+    >
       <VideoCallCard />
       <Logo className="absolute left-4 top-4" />
       <div className="absolute bottom-0 left-0 h-0 w-0">
@@ -34,25 +41,29 @@ export default function VideoCallPanel({ className, ...restProps }: VideoCallPan
           >
             <div className="flex h-full gap-4">
               <VideoCallCard className="rounded-md border border-border flex-shrink-0 w-[300px]" />
-              <div className="flex justify-center items-center h-full flex-grow space-x-2">
-                <Button size="icon" variant="ghost" className="rounded-full w-14 h-14 bg-white/20 hover:bg-white/30 text-white" onClick={() => setMicMute(!isMicMute)}>
-                  <BsMic size="24" className="absolute left-50 top-50" />
-                  {isMicMute &&  
-                    <span className="absolute w-[40px] h-1 bg-red-500 z-50 rotate-[120deg]"></span>
-                  }
-                </Button>
-                <Button size="icon" variant="ghost" className="rounded-full w-24 h-14 bg-white/20 hover:bg-white/30 text-white">
-                  <SkipIcon />
-                  <span className="ml-1 bold">
-                    SKIP
-                  </span>
-                </Button>
-                <Button size="icon" variant="ghost" className="rounded-full w-14 h-14 bg-white/20 hover:bg-white/30 text-white" onClick={() => setHeadsetMute(!isHeadsetMute)}>
-                  <BsHeadset size="24" className="absolute left-50 top-50" />
-                  {isHeadsetMute &&  
-                    <span className="absolute w-[20px] h-1 bg-red-500 z-50 rotate-[120deg]"></span>
-                  }
-                </Button>
+              <div className={cn("flex-grow h-full group-hover:bg-red-500", { "hidden": hideCallControl })}>
+                <div className="flex justify-center items-center h-full space-x-2">
+
+
+                  <Button className="rounded-full w-14 h-14 bg-white/20 hover:bg-white/30 text-white" onClick={() => setMicMute(!isMicMute)}>
+                    <BsMic size="24" className="absolute left-50 top-50" />
+                    {isMicMute &&
+                      <span className="absolute w-[40px] h-1 bg-red-500 z-50 rotate-[140deg]"></span>
+                    }
+                  </Button>
+                  <Button size="icon" variant="ghost" className="rounded-full w-24 h-14 hover:text-white bg-white/20 hover:bg-white/30 text-white">
+                    <SkipIcon />
+                    <span className="ml-1 bold">
+                      SKIP
+                    </span>
+                  </Button>
+                  <Button className="rounded-full w-14 h-14 bg-white/20 hover:bg-white/30 text-white" onClick={() => setHeadsetMute(!isHeadsetMute)}>
+                    <BsHeadset size="24" className="absolute left-50 top-50" />
+                    {isHeadsetMute &&
+                      <span className="absolute w-[20px] h-1 bg-red-500 z-50 rotate-[140deg]"></span>
+                    }
+                  </Button>
+                </div>
               </div>
             </div>
           </PopoverContent>
