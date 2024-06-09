54c120aeedad0e963f3c8e7a65b9949be7b1a0c8
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2024-06-09 19:02:15 +0100

    fix socket connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,24 +2,33 @@ import express from 'express';
 import dotenv from 'dotenv';
 import http from 'http';
 import { Server, Socket } from 'socket.io';
+import cors, { CorsOptions } from 'cors';
 
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
+
+const corsOpts: CorsOptions = {
+	origin: '*',
+	methods: ['GET', 'POST', 'OPTIONS', 'DELETE', 'PATCH'],
+	credentials: true,
+};
+
+app.use(cors(
+	corsOpts,
+));
+
 const io = new Server(server, {
-	cors: {
-		origin: "*",
-		methods: ["GET", "POST"],
-		credentials: true,
-	}
+	cors: corsOpts,
 });
+
 
 io.on('connection', (socket: Socket) => {
 	console.log('a user connected, socket.id:', socket.id);
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
 })
--- frontend/components/global-template.tsx
+++ frontend/components/global-template.tsx
@@ -8,6 +8,7 @@ export default function GlobalTemplate({ children }: { children: React.ReactNode
   useEffect(() => {
     const socket = io("http://localhost:3001", {
       transports: ["websocket"],
+      withCredentials: true,
     })
     socket.on("connect", () => {
       console.log("socket id:", socket.id);
